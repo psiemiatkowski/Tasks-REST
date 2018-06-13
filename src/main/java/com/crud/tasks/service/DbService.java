0c5d6a28e1f701ecc2947577444c2603139b2a8d
unknown <psiemiatkowski@gmail.com>
2018-06-13 21:44:56 +0200

    Task 15.2. v1.00

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,4 +1,23 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class DbService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(Long taskId) {
+        return taskRepository.findAllById(taskId);
+    }
 }
