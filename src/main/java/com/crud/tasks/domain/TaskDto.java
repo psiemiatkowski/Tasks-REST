56853d1361de13d8e7f1f368c7e5c7bfa95f1af4
unknown <psiemiatkowski@gmail.com>
2018-06-13 09:06:01 +0200

    Taks 15.1.

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -3,11 +3,14 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class TaskDto {
     private Long id;
     private String title;
     private String content;
+
 }
