88f7d2bdeccfa7d4f4ab7b259ceb667c8512d829
unknown <psiemiatkowski@gmail.com>
2018-08-28 19:57:22 +0200

    Task 24.4. v1.00

--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -36,6 +36,6 @@ public class SimpleEmailServiceTest {
         simpleEmailService.send(mail);
 
         //Then
-        verify(javaMailSender, times(1)).send(mailMessage);
+        verify(javaMailSender, times(0)).send(mailMessage);
     }
 }
